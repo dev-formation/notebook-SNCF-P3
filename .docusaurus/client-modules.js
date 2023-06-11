4539c8f07cc6abfce88f579078666a1f7b85a853
Jérémy Bojko <jeremy.bojko@protonmail.com>
2023-06-11 18:52:46 +0200

    fix

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/jeremybojko/Documents/Dev/Simplon/SNCF/P3-Montreuil/notebook-SNCF-P3/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/jeremybojko/Documents/Dev/Simplon/SNCF/P3-Montreuil/notebook-SNCF-P3/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/jeremybojko/Documents/Dev/Simplon/SNCF/P3-Montreuil/notebook-SNCF-P3/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/jeremybojko/Documents/Dev/Simplon/SNCF/P3-Montreuil/notebook-SNCF-P3/src/css/custom.css'),
-];
